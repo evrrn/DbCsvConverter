86ce43a1b38690011fa62ac7536a598731d27947
Evrin <ang.elizarova@gmail.com>
2017-11-20 15:47:42 +0300

    1. add connects for setters, transform button, dbtocsv transform 2. clear old code, comments, improve style 3. add tableView

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,8 +2,6 @@
 #include "ui_mainwindow.h"
 #include <QFileDialog>
 #include <QFileInfo>
-#include <QVBoxLayout>
-#include <QtSql>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -12,46 +10,38 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     ui->tableView->setModel(&model);
 
-    /*ui->select_table_lable_1->setVisible(false);
-    ui->edit_table_name_1->setVisible(false);
-    ui->show_table_button_1->setVisible(false);
-    ui->select_table_lable_2->setVisible(false);
-    ui->edit_table_name_2->setVisible(false);
-    ui->show_table_button_2->setVisible(false);*/
+    connect(this, SIGNAL(newDb(QString)), &model, SLOT(setDbName(QString)));
+    connect(this, SIGNAL(newTable(QString)), &model, SLOT(setTableName(QString)));
+    connect(this, SIGNAL(newCsv(QString)), &model, SLOT(setCsvName(QString)));
 
-    connect(ui->dbcsv_button, SIGNAL(clicked(bool)), &model, SLOT(readFromDbToModel()));
-    connect(ui->transform_button, SIGNAL(clicked(bool)), &model, SLOT(writeFromModelToCsv()));
+    connect(this, SIGNAL(newDb(QString)), this, SLOT(names_arent_empty()));
+    connect(this, SIGNAL(newTable(QString)), this, SLOT(names_arent_empty()));
+    connect(this, SIGNAL(newCsv(QString)), this, SLOT(names_arent_empty()));
 
-    connect(ui->transform_button, SIGNAL(clicked(bool)), &model, SLOT(writeFromModelToCsv()));
+    connect(this, SIGNAL(namesArentEmpty(bool)), this, SLOT(enable_transform_button(bool)));
 
-    //QPushButton *b = new QPushButton(this);
+
+    connect(ui->show_from_table_button, SIGNAL(clicked(bool)), &model, SLOT(readFromDbToModel()));
+    connect(this, SIGNAL(transformToCsv()), &model, SLOT(writeFromModelToCsv()));
+
+    // обратно connect(ui->show_to_table_button, SIGNAL(clicked(bool)), &model, SLOT(readFromCsvToModel()));
+    //connect(this, SIGNAL(csvToDb()), &model, SLOT(writeFromModelToDb()));
 
     this->csvtodb_flag = true;
+
+    ui->transform_button->setEnabled(false);
     setVisibleFromTableName(false);
     setVisibleToTableName(false);
-    connect(ui->csvdb_button,SIGNAL(clicked()),this,SLOT(csvdb_button_clicked()));
-    connect(ui->dbcsv_button,SIGNAL(clicked()),this,SLOT(dbcsv_button_clicked()));
-    connect(ui->csvdb_button,SIGNAL(clicked()),this,SLOT(clear_input()));
-    connect(ui->dbcsv_button,SIGNAL(clicked()),this,SLOT(clear_input()));
-    connect(ui->select_from_file_button,SIGNAL(clicked()),this, SLOT(select_from_file_button_clicked()));
-    connect(ui->select_to_file_button,SIGNAL(clicked()),this, SLOT(select_to_file_button_clicked()));
-    connect(ui->show_from_table_button,SIGNAL(clicked()),this,SLOT(show_from_table_button_clicked()));
-    connect(ui->edit_from_table_name,SIGNAL(editingFinished()),this,SLOT(edit_from_table_name_editingFinished()));
-    connect(ui->edit_to_table_name,SIGNAL(editingFinished()),this,SLOT(edit_to_table_name_editingFinished()));
-    connect(ui->transform_button,SIGNAL(clicked()),this,SLOT(transform_button_clicked()));
-    ui->transform_button->setEnabled(this->condition_checker());
-    view_result_table = new QTableView;
-    //sdb = QSqlDatabase::addDatabase("QSQLITE");
-    //view_result_table->setGeometry(100,100,100,100);
-    ui->horizontalLayout_9->addWidget(view_result_table);
-            //QPushButton *b = new QPushButton(this);
-    //QVBoxLayout *layout = new QVBoxLayout(this);
-    //layout->addWidget(b);
-    //layout->addWidget(listtable);
-    //b->setText(QString("TEST"));
-    //ui->gridLayout->addWidget(b);
-    //ui->centralWidget->addWidget(&listtable);
 
+    connect(ui->csvdb_button, SIGNAL(clicked()), this, SLOT(csvdb_button_clicked()));
+    connect(ui->dbcsv_button, SIGNAL(clicked()), this, SLOT(dbcsv_button_clicked()));
+
+    connect(ui->select_from_file_button, SIGNAL(clicked()), this, SLOT(select_from_file_button_clicked()));
+    connect(ui->select_to_file_button, SIGNAL(clicked()), this, SLOT(select_to_file_button_clicked()));
+
+    connect(ui->edit_from_table_name, SIGNAL(editingFinished()), this, SLOT(edit_from_table_name_editingFinished()));
+    connect(ui->edit_to_table_name, SIGNAL(editingFinished()), this, SLOT(edit_to_table_name_editingFinished()));
+    connect(ui->transform_button, SIGNAL(clicked()), this ,SLOT(transform_button_clicked()));
 }
 
 MainWindow::~MainWindow()
@@ -59,12 +49,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::setVisibleFromTableName(bool flag){
+void MainWindow::setVisibleFromTableName(bool flag)
+{
     ui->select_from_table_lable->setVisible(flag);
     ui->edit_from_table_name->setVisible(flag);
     ui->show_from_table_button->setVisible(flag);
 }
-void MainWindow::setVisibleToTableName(bool flag){
+
+void MainWindow::setVisibleToTableName(bool flag)
+{
     ui->select_to_table_lable->setVisible(flag);
     ui->edit_to_table_name->setVisible(flag);
     ui->show_to_table_button->setVisible(flag);
@@ -72,146 +65,122 @@ void MainWindow::setVisibleToTableName(bool flag){
 
 void MainWindow::csvdb_button_clicked()
 {
-    if (!this->csvtodb_flag){
+    if (!this->csvtodb_flag)
+    {
         this->csvtodb_flag = true;
         ui->select_from_file_label->setText(QString("Выберите файл .csv"));
         ui->select_to_file_label->setText(QString("Выберите файл базы"));
-        //очистка полей после изменения типа
+        clear_input();
     }
-    ui->transform_button->setEnabled(this->condition_checker());
 }
 
 void MainWindow::dbcsv_button_clicked()
 {
-    //emit valueChanged();
-    //this->convert_flag = false;
-    if (this->csvtodb_flag){
+    if (this->csvtodb_flag)
+    {
         this->csvtodb_flag = false;
         ui->select_from_file_label->setText(QString("Выберите файл базы"));
         ui->select_to_file_label->setText(QString("Выберите файл .csv"));
+        clear_input();
     }
-    ui->transform_button->setEnabled(this->condition_checker());
 }
 
 void MainWindow::select_from_file_button_clicked()
 {
-    if(this->csvtodb_flag){
+    if (this->csvtodb_flag)
+    {
         csv_file_name = QFileDialog::getOpenFileName(0, "Open Dialog", "", "*.csv");
-        ui->file_from_name->setText(QFileInfo(csv_file_name).fileName());
+        ui->file_from_label->setText(QFileInfo(csv_file_name).fileName());
+        emit newCsv(csv_file_name);
     }
-    else{
+    else
+    {
         db_file_name = QFileDialog::getOpenFileName(0, "Open Dialog", "", "");
-        ui->file_from_name->setText( QFileInfo(db_file_name).fileName());
+        ui->file_from_label->setText( QFileInfo(db_file_name).fileName());
         ui->edit_from_table_name->clear();
-        if(db_file_name != ""){
+
+        if (db_file_name != "")
             setVisibleFromTableName(true);
-        }
-        else{
+        else
             setVisibleFromTableName(false);
-        }
+
+        emit newDb(db_file_name);
     }
-    ui->transform_button->setEnabled(this->condition_checker());
 }
 
 void MainWindow::select_to_file_button_clicked()
-{   if(this->csvtodb_flag){
-        db_file_name = QFileDialog::getOpenFileName(0, "Open Dialog", "", "");
-        ui->file_to_name->setText(QFileInfo(db_file_name).fileName());
-        ui->edit_to_table_name->clear();
-        if(db_file_name != ""){
-            setVisibleToTableName(true);
-        }
-        else{
-            setVisibleToTableName(false);
-        }
-        /***
-        QSqlDatabase sdb = QSqlDatabase::addDatabase("QSQLITE");
-        sdb.setDatabaseName(open_file);
-        if (!sdb.open())
-            qDebug() << sdb.lastError().text();
-        this->listtable.addItems(sdb.tables());
-        listtable.show();
-        */
-
-
-    }
-    else{
-        csv_file_name = QFileDialog::getSaveFileName(0, "Open Dialog", "");
-        ui->file_to_name->setText(QFileInfo(csv_file_name).fileName());
-
-    }
-    ui->transform_button->setEnabled(this->condition_checker());
-}
-
-void MainWindow::show_from_table_button_clicked()
 {
-    //view table
-/*
-    //QSqlDatabase sdb = QSqlDatabase::addDatabase("QSQLITE");
-    this->sdb.setDatabaseName(db_file_name);
-    if (!sdb.open())
-        qDebug() << sdb.lastError().text();
-    this->listtable.addItems(sdb.tables());
-    //listtable.show();
+    if (this->csvtodb_flag)
+    {
+        db_file_name = QFileDialog::getOpenFileName(0, "Open Dialog", "", "");
+        ui->file_to_label->setText(QFileInfo(db_file_name).fileName());
+        ui->edit_to_table_name->clear();
 
-    QSqlTableModel *model = new QSqlTableModel(this, sdb);
-       model->setTable(table_name);
-       //this->view_result_table = new QTableView;
-       view_result_table->setModel(model);
-       //view->show();
-*/
-    //Проверить есть ли данное имя в базе
+        if(db_file_name != "")
+            setVisibleToTableName(true);
+        else
+            setVisibleToTableName(false);
+
+        emit newDb(db_file_name);
+    }
+    else
+    {
+        csv_file_name = QFileDialog::getSaveFileName(0, "Open Dialog", "");
+        ui->file_to_label->setText(QFileInfo(csv_file_name).fileName());
+        emit newCsv(csv_file_name);
+    }
 }
 
 void MainWindow::edit_from_table_name_editingFinished()
 {
     table_name = ui->edit_from_table_name->text();
-    ui->transform_button->setEnabled(this->condition_checker());
+    emit newTable(table_name);
 }
 
 void MainWindow::edit_to_table_name_editingFinished()
 {
     table_name = ui->edit_to_table_name->text();
-    ui->transform_button->setEnabled(this->condition_checker());
+    emit newTable(table_name);
 }
-/*
-void MainWindow::on_edit_table_name_2_textChanged(const QString &arg1)
-{
-    table_name = arg1;
-}
-*/
+
 void MainWindow::clear_input(){
     setVisibleFromTableName(false);
     setVisibleToTableName(false);
-    ui->file_from_name->clear();
-    ui->file_to_name->clear();
+
+    ui->file_from_label->clear();
+    ui->file_to_label->clear();
     ui->edit_from_table_name->clear();
     ui->edit_to_table_name->clear();
-    this->csv_file_name="";
-    this->db_file_name="";
-    this->table_name="";
-    this->view_result_table->setModel(0);
-    this->view_result_table->setHorizontalHeader(0);
-    this->view_result_table->setVerticalHeader(0);
-    ui->transform_button->setEnabled(this->condition_checker());
+
+    this->csv_file_name = "";
+    emit newCsv(csv_file_name);
+    this->db_file_name = "";
+    emit newDb(db_file_name);
+    this->table_name = "";
+    emit newTable(table_name);
+
+    model.clearTable();
 }
 
 
 void MainWindow::transform_button_clicked()
 {
-    //csvtodb
-    if(this->csvtodb_flag){
-
-    }
-    //dbtocsv
-    else{
-
-    }
+    if (this->csvtodb_flag)
+        emit transformToDb();
+    else
+        emit transformToCsv();
 }
 
-bool MainWindow::condition_checker(){
-    if(csv_file_name!="" && db_file_name!="" && table_name!="")
-        return true;
-    else return false;
+void MainWindow::enable_transform_button(bool enable)
+{
+    ui->transform_button->setEnabled(enable);
+}
 
+void MainWindow::names_arent_empty()
+{
+    if (csv_file_name != "" && db_file_name != "" && table_name != "") {
+        emit namesArentEmpty(true);
+        return;
+    }
+    emit namesArentEmpty(false);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -7,9 +7,6 @@
 #include <QFileDialog>
 #include <QString>
 #include <QLabel>
-#include <QListWidget>
-#include <QSqlTableModel>
-#include <QTableView>
 
 namespace Ui {
 class MainWindow;
@@ -22,21 +19,16 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
     bool csvtodb_flag;
-    QListWidget listtable;
-    //true = csv -> db
-    //false = db -> csv
-    //QSqlTableModel  *model;
-    //DataBase        *db;
-    //Временные имена, которые будут храниться в модели
+
     QString csv_file_name;
     QString db_file_name;
     QString table_name;
-    QTableView *view_result_table;
-    //QSqlDatabase sdb;
+
     void setVisibleFromTableName(bool flag);
     void setVisibleToTableName(bool flag);
-    bool condition_checker();
+
 private slots:
     void csvdb_button_clicked();
     void dbcsv_button_clicked();
@@ -44,20 +36,24 @@ private slots:
     void select_from_file_button_clicked();
     void select_to_file_button_clicked();
 
-    void show_from_table_button_clicked();
-
     void edit_from_table_name_editingFinished();
-
     void edit_to_table_name_editingFinished();
-
-
-    //void on_edit_table_name_2_textChanged(const QString &arg1);
 
     void transform_button_clicked();
     void clear_input();
+
+    void names_arent_empty();
+    void enable_transform_button(bool enable);
+
 signals:
-    //void choose_item_from_list();
-   // void valueChanged();
+    void newCsv(QString);
+    void newDb(QString);
+    void newTable(QString);
+
+    void transformToDb();
+    void transformToCsv();
+
+    void namesArentEmpty(bool);
 
 private:
     Ui::MainWindow *ui;
@@ -65,14 +61,3 @@ private:
 };
 
 #endif // MAINWINDOW_H
-
-/*
-1.+ Сделать явные connect
-2.+ Переименовать кнопки выбора файла  from, to
-3.+ Переставить местами поля ввода для имени таблицы(в начале идет 2, а потом 1, ето не круто)
-4. условие для активации кнопки "Преобразовать" и "Вывод таблицы"
-5.+ окно для вывода результирующей модели
-5.1. size of block
-6.+ clear-function
-6.1. clear model
-*/
